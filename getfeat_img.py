fefa38ee1e43c423ef001e511c410e5f9319e5ec
Aleksandr Jermakov <39992844+AlexBlck@users.noreply.github.com>
2020-06-26 00:13:20 +0100

    Update getfeat_img.py

--- getfeat_img.py
+++ getfeat_img.py
@@ -50,29 +50,7 @@ def extractitem(net, mean_pixel, im_array):
     DATA_LAYER = net.inputs[0]
     net.blobs[DATA_LAYER].reshape(1,3,224,224) 
     try:
-       input_image = im_array#.resize((256,256),Image.BILINEAR).crop((16,16,240,240))
-       #resize
-       sf = 256.0/max(input_image.size)
-       input_image = input_image.resize((int(input_image.width*sf),int(input_image.height*sf)),Image.BILINEAR)
-       pw = (256 - input_image.width)/2
-       ph = (256 - input_image.height)/2
-       input_image = np.array(input_image)
-         
-       #make sure image is RGB format
-       if input_image.ndim == 2:
-         input_image = input_image[...,None]
-         input_image = np.repeat(input_image,3, axis=2)
-          
-       #pad to make 256x256
-       input_image = np.pad(input_image,((ph,ph),(pw,pw),(0,0)), mode='edge')
-         
-       #crop
-       input_image = input_image[16:240,16:240]
-       
-       sys.stdout.flush()
-       transformed_image = np.array(input_image,dtype=np.float32)[:,:,::-1].transpose(2,0,1) - mean_pixel
-       sys.stdout.flush()
-       net.blobs[DATA_LAYER].data[...] = transformed_image
+       net.blobs[DATA_LAYER].data[...] = im_array
        sys.stdout.flush()
        _ = net.forward()
        sys.stdout.flush()
@@ -82,7 +60,7 @@ def extractitem(net, mean_pixel, im_array):
     
     except Exception as e:
        s=str(e)
-       print('WARNING: Image was unusable %s' % fname)
+       print('WARNING: Image was unusable')
        print(s)
        prediction = np.zeros(LAYER_DIMS).astype(np.float32)
     
